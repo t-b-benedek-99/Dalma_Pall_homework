4e67a2c3d48b8fc0cfed0e11c3bc2f7302a3abc4
Dalma Pall <pall.dalma@gmail.com>
2020-12-28 19:18:39 +0100

    is-in-list.ts added

---
+++ is-in-list.ts
@@ -0,0 +1,19 @@
+let listOfNumbers: number[] = [2, 4, 6, 8, 10, 12, 14, 16];
+
+function checkNums (array: number []):boolean {
+
+    
+    if (array.includes (4 && 8 && 12 && 16) ){
+        return true
+    }
+    else return false;
+}
+
+console.log(checkNums(listOfNumbers));
+
+//TESTING
+
+let array2: number [] = [2, 3, 4, 5, 6];
+
+
+console.log(checkNums(array2));
