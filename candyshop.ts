8a4af0ae079353d1a7bd347f1059feb3258c1ce2
Dalma Pall <pall.dalma@gmail.com>
2020-12-28 18:55:18 +0100

    candyshop.ts was added

---
+++ candyshop.ts
@@ -0,0 +1,10 @@
+let shopItems: any[] = ['Cupcake', 2, 'Brownie', false];
+
+function sweets (array: any) {
+    array.splice (1, 1, ("Croissant"));
+    array.splice (3, 1, ("Ice cream"));
+
+    return array
+}
+
+console.log (sweets (shopItems));
