c8000372b3d48d5a759911fa2946e944b7e7fec4
palldalma <74419536+palldalma@users.noreply.github.com>
2021-01-15 00:36:30 +0100

    Delete shopping_list.ts
    in the data structures folder

--- shopping_list.ts
+++
@@ -1,4 +0,0 @@
-let shoppingList : string [] = ["eggs", "milk", "fish", "apples", "bread" , "chicken"]; 
-
-console.log (shoppingList.includes ("milk"));
-console.log (shoppingList.includes ("bananas"));
