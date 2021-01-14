cf01d7c05a27fc57300f90aeb1be71828969440b
palldalma <74419536+palldalma@users.noreply.github.com>
2021-01-15 00:31:55 +0100

    Delete solar_system.ts
    in the data structures folder

--- solar_system.ts
+++
@@ -1,9 +0,0 @@
-let planetList: string[] = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune"];
-
-function putSaturn (inputArray: string []) {
-
-    inputArray.splice (5,0, "Saturn");
-    return inputArray;
-}
-
-console.log (putSaturn (planetList));
